109e9d64d15ea1782d409bc56ace9f59d80491d2
prachit082 <20scs047.prachit.16@gmail.com>
2025-06-07 18:16:33 +0530

    Added all views for student crud and route

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -82,18 +82,16 @@ div.content {
         <div class="col-md-3">
           <div class="sidebar">
             <a class="active" href="#home">Home</a>
-            <a href="#news">Student</a>
-            <a href="#contact">Teacher</a>
-            <a href="#about">Courses</a>
-            <a href="#contact">Enrollment</a>
-            <a href="#about">Payment</a>
+            <a href="{{ url('/students') }}">Student</a>
+            <a href="{{ url('/teachers') }}">Teacher</a>
+            <a href="{{ url('/courses') }}">Courses</a>
+            <a href="{{ url('/enrollments') }}">Enrollment</a>
+            <a href="{{ url('/payments') }}">Payment</a>
           </div>
         </div>
 
         <div class="col-md-9">
-          <div class="content">
             @yield('content')
-          </div>
         </div>
     </div>
 </div>
